b218607b5c139f3cf49c333884798a77b3845838
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2023-01-08 12:40:47 +0530

    Flife Cycle Demo

--- Angular/Facebook/src/app/a4/a4.component.ts
+++ Angular/Facebook/src/app/a4/a4.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonServDemoService } from '../common-serv-demo.service';
 
 @Component({
@@ -6,11 +6,19 @@ import { CommonServDemoService } from '../common-serv-demo.service';
   templateUrl: './a4.component.html',
   styleUrls: ['./a4.component.css']
 })
-export class A4Component implements OnInit{
+export class A4Component implements OnInit,OnDestroy{
   myemp:any=[];
   constructor(private serv:CommonServDemoService){}
+ 
+  // getEmp(){
+  //   this.myemp =this.serv.emp;
+  // }
   ngOnInit(): void {
    this.myemp =this.serv.emp;
+  }
+
+  ngOnDestroy(): void {
+   // close the connection 
   }
 
 }
